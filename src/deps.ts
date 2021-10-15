39ba7468419bfc1a781251feb3cdc04a340accc2
Suzu Tomo <kamekyame@outlook.com>
2021-10-15 20:42:46 +0900

    Bump: kakomimasu/server from v1.0.0-beta.0 to v1.0.0-beta.2

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/kakomimasu/server/v1.0.0-beta.0/api/types.ts";
+export * from "https://raw.githubusercontent.com/kakomimasu/server/v1.0.0-beta.2/v1/types.ts";
