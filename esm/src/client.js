5c7f5e47501b6222f8d556470bd247c56057c2e3
Suzu Tomo <kamekyame@outlook.com>
2021-11-01 00:28:33 +0900

    dnt

--- esm/src/client.js
+++ esm/src/client.js
@@ -127,9 +127,9 @@ export default class ApiClient {
             return { success: res.status === 200, data: yield res.json(), res };
         });
     }
-    gameCreate(data) {
+    gameCreate(data, auth) {
         return __awaiter(this, void 0, void 0, function* () {
-            const res = yield this._fetchPostJson("/v1/game/create", data);
+            const res = yield this._fetchPostJson("/v1/game/create", data, auth);
             return { success: res.status === 200, data: yield res.json(), res };
         });
     }
