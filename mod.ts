0dfcc42932261ce686998503a65a9083f3587939
ninja03 <69106571+ninja03@users.noreply.github.com>
2022-05-29 23:26:06 +0900

    Update mod.ts
    Co-authored-by: SuzuTomo <47011206+kamekyame@users.noreply.github.com>

--- mod.ts
+++ mod.ts
@@ -1,2 +1,3 @@
 export * from "https://cdn.jsdelivr.net/gh/kakomimasu/server@v1.0.0-beta.8/client/client.ts";
-export { default as ApiClient } from "https://cdn.jsdelivr.net/gh/kakomimasu/server@v1.0.0-beta.8/client/client.ts";
+import { default as ApiClient } from "https://cdn.jsdelivr.net/gh/kakomimasu/server@v1.0.0-beta.8/client/client.ts";
+export default ApiClient; 
