c941570d4c983d66f3909d38acbf36a3e105b60d
kamekyame <kamekyame@outlook.com>
2025-05-07 21:00:18 +0900

    🚿 古いファイルを削除

--- types/mod.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./deps/cdn_jsdelivr_net_gh_kakomimasu/client/client.js";
-import { default as ApiClient } from "./deps/cdn_jsdelivr_net_gh_kakomimasu/client/client.js";
-export default ApiClient;
